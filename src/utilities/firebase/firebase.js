886608174bf4bc1682e38ae705223a4da2e048cd
Richard <rkerr022@gmail.com>
2026-02-16 20:08:23 -0500

    updating the with proper measure when having sensitive data

--- src/utilities/firebase/firebase.js
+++ src/utilities/firebase/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDlmibnyWDU2Nw2Wjz_HNmfKHtsVwaUjek",
-  authDomain: "acme-store-db.firebaseapp.com",
-  projectId: "acme-store-db",
-  storageBucket: "acme-store-db.firebasestorage.app",
-  messagingSenderId: "109395159979",
-  appId: "1:109395159979:web:218f71fcea27c26a7d00ee"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN, 
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
